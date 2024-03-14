1ae810737fe39340a06df24d46b59ed9e57f2ba3
Ekaterina Rykova <katrykova2002@yandex.ru>
2024-03-14 14:05:02 +0300

    add helmet

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT ||3000);
+  // app.use(helmet());
+  app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'"],
+        connectSrc: ["'self'", "https://travel-lac-six.vercel.app/"],
+      },
+    })
+  );
 }
 bootstrap();
